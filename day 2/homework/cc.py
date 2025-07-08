b2de4e82ffca660dcb73b2833f2677faef580ce9
gioaptsi <gioaptsiauri51@gmail.com>
2025-07-08 14:02:29 +0400

    second commit

---
+++ day 2/homework/cc.py
@@ -0,0 +1,46 @@
+num1 = 5
+num2 = 15
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+n1 =("mersedes")
+n2 =("cat")
+n3 =("georgia")
+n4 =("gio")
+
+n5 =(15)
+n6 =(20)
+n7 =(10)
+n8 =(2)
+
+n9 =(10,50)
+n10 =(5,80)
+n11 =(3,40)
+n12 =(12,30)
+
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
+print(n8)
+print(n9)
+print(n10)
+print(n11)
+print(n12)
+
+# ამ მოქმედებას ჰქვია კონკატინაცია
+saxeli1 =("giorgi")
+saxeli2 =("goga")
+print(saxeli1 + saxeli2)
+
+cvladi1=("gio")
+cvladi2=("zura")
+cvladi3=("nugo")
+cvladi4=("nika")
+cvladi5=("sandro")
+print(cvladi1 + cvladi2 + cvladi3 + cvladi4 +cvladi5)
