93d70ec537b290a1ec02b51181a2e94af1dfc27c
gioaptsi <gioaptsiauri51@gmail.com>
2025-07-14 15:20:07 +0400

    1

--- day04/homework/hh.py
+++ day04/homework/hh.py
@@ -1,5 +1,39 @@
+#2
 #intup არის ინფორმაცია, რომელსაც აძლევ კომპიუტერს.
 #outpu არის შედეგი, რომელსაც კომპიუტერი გაჩვენებს.
 #მაგალითად 
 name = input("whats your name? " )
 print(name)
+
+#3
+surname=input("whats your surname? ")
+print(type(surname))
+
+#4
+#სტრინგი
+name="nino"
+city="tbilisi"
+lenguage="georgian"
+color="green"
+food="xinkali"
+
+age=25
+year=2025
+students=30
+books=12
+hours=2
+
+height=1.80
+temperature=36.6
+price=12.99
+weight=60.5
+speed=5.6
+
+#5
+name="tamari"
+age=16
+height=1.65
+
+print(type(name))
+print(type(age))
+print(type(height))
