03fbb1b79e60aeb2171bbee295dfc8a3a56767fb
gioaptsi <gioaptsiauri51@gmail.com>
2025-07-22 19:55:42 +0400

    1

--- day05/homework/bb.py
+++ day05/homework/bb.py
@@ -5,3 +5,37 @@ num2=(10)
 print(num1>num2)
 print(num1<num2)
 print(num1==num2)
+
+name_1=("gio")
+name_2=("nika")
+name_3=("zura")
+name_4=("gocha")
+name_5=("nugo")
+print(name_1+name_2+name_3+name_4+name_5)
+
+score_1=(int(input("enter your score")))
+score_2=(int(input("enter your score")))
+score_3=(int(input("enter your score")))
+score_4=(int(input("enter your score")))
+print(score_1+score_2+score_3+score_4)
+
+car=("bmw")
+number=(5)
+height=(1,60)
+honesty=("true")
+print(type(car))
+print(type(number))
+print(type(height))
+print(type(honesty))
+
+name1="gio"
+name2="nika"
+print(name1>name2)
+print(name1<name2)
+print(name1==name2)
+
+age_1=int(2)
+age_2=int(9)
+age_3=int(10)
+age_4=int(5)
+print(age_1+age_2+age_3+age_4)
