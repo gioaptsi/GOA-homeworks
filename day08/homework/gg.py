423d674acee3b10d57a2742af2995b04ae7f96a1
gioaptsi <gioaptsiauri51@gmail.com>
2025-07-31 17:47:39 +0400

    1

--- day08/homework/gg.py
+++ day08/homework/gg.py
@@ -1,3 +1,63 @@
+#1
 print(8<2 or 10==11)
 print(50==50 and 5>10)
 print(70==30 and 30>20)
+print(80>100 and 100==100)
+print(50<10 or 100==12)
+
+print(60==60 or 50>70)
+print(50==50 and 70==70)
+print(30==20 or 4>1)
+print(60<100 and 70>50)
+print(80==80 or 30<100)
+
+#2
+#sequancing არის თანმიმდევრობა 
+#iteration არის გამეორება
+#selection არის არჩევა
+
+#3
+name="gio"
+name="gela"
+print(name)
+
+#4
+#loop არის გამეორება და იგი გვეხმარება ერთი და იგივე რამ დავწეროთ რამდენჯერაც გვინდა
+
+#5
+#range() ფუნქციას გადაეცემა რამდენჯერ გამეორდება რიცხვი ან სახელი 
+
+#6
+for i in range(1):
+    print("audi")
+
+#7
+for i in range(100):
+    print("aptsiauri")
+
+#8
+for i in range(46):
+    print("yviteli")
+
+#9
+for i in range(32):
+    print("g")
+
+#10
+w=input("choose any name")
+e=input("choose another name")
+r=input("choose third name")
+t=input("choose number")
+
+print(w+e+r+t)
+
+#11
+name="gio"
+age=20
+height=1,80
+boolean= (50==12 or 70>50)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(boolean))
