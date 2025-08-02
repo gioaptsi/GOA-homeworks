66751d7a61ecfdf8e3a42893802a590fee021599
gioaptsi <gioaptsiauri51@gmail.com>
2025-08-02 18:55:49 +0400

    1

---
+++ day06/homework/mmm.py
@@ -0,0 +1,18 @@
+num1=int(input("enter number"))
+num2=int(input("enter second number"))
+
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+
+str1="70"
+str2="50"
+str3="20"
+int1=90
+int2=40
+print((int(str1)* int(str2)* int(str3)* int1* int2)/5)
+
+word1=input("enter first word")
+word2=input("enter second word")
+word3=input("enter third word")
+int1= int(input("enter your number"))
