a336dd1d88f8747ce530b945ae22333478087ae0
gioaptsi <gioaptsiauri51@gmail.com>
2025-08-11 23:08:08 +0400

    1

--- day10/homework/yyyyyy.py
+++ day10/homework/yyyyyy.py
@@ -1,4 +1,4 @@
-for i in range(38):
+for i in range(39):
     print(i)
 
 for i in range(30,40):
@@ -9,4 +9,19 @@ for i in range(12,120):
 
 for i in range(10,100,3):
     print(i)
+
+for i in range(1,31):
+    print(str(i)+ "apple")
+
+for i in range(13):
+    age=(int(input("enter your name")))
+
+i=0
+while i<10:
+    print("gio")
+    i=i+1
+
+while i<24:
+    print(i)
+    i=i+1
 
